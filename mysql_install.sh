a2c1b4be4a08d0880424a8bb58c48c665cdc1d5f
root <root@stationServer.(none)>
2016-01-29 23:14:05 +0800

    first

---
+++ mysql_install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+user=mysql
+group=mysql
+
+#create group if not exists
+egrep "^$group" /etc/group >& /dev/null
+if [ $? -ne 0 ]
+then
+    groupadd $group
+fi
+
+#create user if not exists
+egrep "^$user" /etc/passwd >& /dev/null
+if [ $? -ne 0 ]
+then
+    useradd -g $group $user
+fi
+
+cur_dir=$(cd "$(dirname "$0")"; pwd)
+
+data_dir=/htdata/mysql
+src_dir=/usr/local/src
+bin_dir=/usr/local/mysql
+yum install git
+yum install -y gcc gcc-c++  make
+yum install –y openssl openssl-devel ncurses ncurses-devel cmake
+ 
+
+cd $src_dir 
+
+if [ ! -f $src_dir/mysql-5.5.41.tar.gz ] ; then 
+	wget https://downloads.mariadb.com/archives/mysql-5.5/mysql-5.5.41.tar.gz
+fi
+
+if [ ! -d $src_dir/mysql-5.5.41 ] ; then
+	tar	-zxvf mysql-5.5.41.tar.gz
+fi
+
+if [ ! -e $bin_dir/bin/mysqld_safe ] ; then
+	cd $src_dir/mysql-5.5.41
+	cmake . -DCMAKE_INSTALL_PREFIX=$bin_dir -DMYSQL_DATADIR=$data_dir
+	make && make install
+fi
+
+if [ ! -e $bin_dir/bin/mysqld_safe ] ; then
+	echo "fail to install mysql"
+	exit;
+fi
+ 
+cd $data_dir
+chown -R mysql $data_dir
+chgrp -R mysql .
+
+
+cp $cur_dir/init_sh/mysqld /etc/init.d/
+cp $cur_dir/sys_config/my.cnf /etc/
+chown root.root /etc/rc.d/init.d/mysqld 
+
+
+cd /usr/local/mysql 
+chown -R root .  
+scripts/mysql_install_db --user=mysql
+$bin_dir/bin/mysqld_safe --user=mysql
+
+$bin_dir/bin/mysqladmin -u root password '123456'  
+ 
+chkconfig --add mysqld   
+chkconfig --level 3 mysqld on
+chkconfig --level 5 mysqld on 
+
+echo 'export PATH=$PATH:/usr/local/mysql/bin' >> /etc/profile
+source /etc/profile
+
+
+mysql -u root -p -e "SET PASSWORD FOR 'root'@'localhost'=PASSWORD('123456');"
---
+++ nginx_install.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+src_dir=/usr/local/src
+cur_dir=$(cd "$(dirname "$0")"; pwd)
+yum install git wget
+yum -y install gcc gcc-c++  make automake autoconf  pcre pcre-devel
+
+# command -v git >/dev/null 2>&1 || { yum install  git}
+# command -v wget >/dev/null 2>&1 || { yum install  wget}
+ 
+cd $src_dir
+
+if [ ! -f $src_dir/LuaJIT-2.0.3.tar.gz ] ; then
+	`wget  http://luajit.org/download/LuaJIT-2.0.3.tar.gz`
+fi
+
+if [ ! -d $src_dir/LuaJIT-2.0.3 ] ; then
+	tar -zxvf LuaJIT-2.0.3.tar.gz
+fi 
+
+if [ ! -d /usr/local/luajit ] ; then
+	cd $src_dir/LuaJIT-2.0.3
+	make &&  make install PREFIX=/usr/local/luajit
+fi 
+cd $src_dir
+  
+export LUAJIT_INC=/usr/local/luajit/include/luajit-2.0/
+export LUAJIT_LIB=/usr/local/luajit/lib
+
+
+
+#
+cd $src_dir
+git clone    https://github.com/simpl/ngx_devel_kit 
+git clone    https://github.com/agentzh/echo-nginx-module  
+git clone    https://github.com/chaoslawful/lua-nginx-module 
+git clone    https://github.com/yaoweibin/nginx_tcp_proxy_module
+
+cd $src_dir
+
+
+if [ ! -e $src_dir/nginx-1.9.9.tar.gz ] ; then
+	wget http://nginx.org/download/nginx-1.9.9.tar.gz
+fi
+
+if [ ! -d $src_dir/nginx-1.9.9 ] ; then
+	tar	-zxvf nginx-1.9.9.tar.gz
+fi 
+
+cd $src_dir/nginx-1.9.9
+patch -p1 < /usr/local/src/nginx_tcp_proxy_module/tcp.patch
+
+
+./configure --sbin-path=/usr/local/nginx/nginx \
+--conf-path=/usr/local/nginx/nginx.conf \
+--pid-path=/usr/local/nginx/nginx.pid \
+--add-module=/usr/local/src/ngx_devel_kit \
+--add-module=/usr/local/src/echo-nginx-module \
+--add-module=/usr/local/src/lua-nginx-module \
+--with-http_ssl_module \
+--with-http_stub_status_module \
+--with-http_sub_module
+
+make &&  make install
+
+if [ ! -e /usr/local/nginx/nginx ] ; then
+	echo ''
+	
+	exit
+fi
+
+
+ln -s /usr/local/luajit/lib/libluajit-5.1.so /lib64/libluajit-5.1.so.2
+
+cd `dirname $0`
+
+cp $cur_dir/init.d/nginx /etc/init.d/
+
+echo "alias nginxlog='cd /usr/local/nginx/logs'" >> /root/.bashrc 
+source /root/.bashrc 
