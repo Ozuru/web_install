f67aaca7548419337785d830b870bd147f557925
lilien1010 <lilien1010@gmail.com>
2016-05-13 11:02:26 +0800

    fix xtra

--- xtra_backup.sh
+++ xtra_backup.sh
@@ -3,14 +3,12 @@ cur_dir=$(cd "$(dirname "$0")"; pwd)
 
 database_passwd=$1
 backup_dir=$2
-yum install git wget
-yum install -y gcc gcc-c++  make
- echo $1
- exit;
+#yum install git wget
+#yum install -y gcc gcc-c++  make
 per_cnt=`yum list | grep percona | wc -l`
 
 if [ $per_cnt -gt 0 ] ;then
-    yum install http://www.percona.com/downloads/percona-release/redhat/0.1-3/percona-release-0.1-3.noarch.rpm
+    yum install -y http://www.percona.com/downloads/percona-release/redhat/0.1-3/percona-release-0.1-3.noarch.rpm
 fi
 
 
@@ -31,6 +29,6 @@ yum install percona-xtrabackup
 
 innobackupex --user=root --password=$database_passwd $backup_dir
 
-sec_dir=`ls -tcl /home/git/web_install/ | grep '^d' | head -1 | awk '{print $NF}'`
+sec_dir=`ls -tcl $backup_dir | grep '^d' | head -1 | awk '{print $NF}'`
 
 innobackupex --user=root --password=$database_passwd --apply-log $backup_dir/$sec_dir
